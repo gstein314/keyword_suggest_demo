22d7864e6685b4f9621ae320fcd5957748f24277
行事光 <h.gyoji.vrds@gmail.com>
2024-05-20 22:15:22 +0900

    fix main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,33 @@
 import { keywordSuggest } from './keywordSuggest.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const inputBox1 = document.getElementById('inputBox1');
-  const inputBox2 = document.getElementById('inputBox2');
-  const inputBox3 = document.getElementById('inputBox3');
+// API（もしかして検索）あり、キーワード選択欄なし
+keywordSuggest('NANDO', '../tsv/NANDO_sample.tsv', {
+  api_url: 'http://localhost:5555/moshikashite_test_api?text=',
+});
 
-  inputBox1.addEventListener('input', () => {
-    keywordSuggest('inputBox1', '../tsv/NANDO.tsv', {
-      api_url: 'http://localhost:5555/moshikashite_test_api?text=',
-    });
-  });
+// API（もしかして検索）あり、キーワード選択欄あり
+keywordSuggest('MONDO', '../tsv/mondo_sample.tsv', {
+  api_url: 'http://localhost:5555/moshikashite_test_api?text=',
+  includeNoMatch: true,
+});
 
-  inputBox2.addEventListener('input', () => {
-    keywordSuggest('inputBox2', '../tsv/mondo_sample.tsv', {
-      api_url: 'http://localhost:5555/moshikashite_test_api?text=',
-      includeNoMatch: true,
-    });
-  });
+// API（もしかして検索）なし、キーワード選択欄なし
+keywordSuggest('ICD10', '../tsv/icd10_sample.tsv');
 
-  inputBox3.addEventListener('input', () => {
-    keywordSuggest('inputBox3', '../tsv/icd10_sample.tsv');
-  });
-
-  document.addEventListener('selectedLabel', function (event) {
-    const inputBoxId = event.detail.inputBoxId;
-    const labelInfo = event.detail.labelInfo;
-    switch (inputBoxId) {
-      case 'inputBox1':
-        console.log('input box1:', labelInfo);
-        break;
-      case 'inputBox2':
-        console.log('input box2:', labelInfo);
-        break;
-      case 'inputBox3':
-        console.log('input box3:', labelInfo);
-        break;
-      default:
-        console.error('Unknown input box ID:', inputBoxId);
-    }
-  });
+document.addEventListener('selectedLabel', function (event) {
+  const inputBoxId = event.detail.inputBoxId;
+  const labelInfo = event.detail.labelInfo;
+  switch (inputBoxId) {
+    case 'NANDO':
+      console.log('NANDO:', labelInfo);
+      break;
+    case 'MONDO':
+      console.log('MONDO:', labelInfo);
+      break;
+    case 'ICD10':
+      console.log('ICD10:', labelInfo);
+      break;
+    default:
+      console.error('Unknown input box ID:', inputBoxId);
+  }
 });
